f4df5627f557a06aa8aabede4a8dc148fe3e40ad
ismaildude <38566647+ismaildude@users.noreply.github.com>
2020-06-20 23:32:32 -0700

    Update main.r

--- main.r
+++ main.r
@@ -49,7 +49,6 @@ LinearExpression <- function(Vector){
       TempVector <- c(TempVector,CurrValue)
     }
   }
-  print(TempVector)
   return(TempVector)
 }
 
@@ -65,7 +64,6 @@ SigmoidExpression <- function(Vector){
       TempVector <- c(TempVector,CurrValue)
     }
   }
-  print(TempVector)
   return(TempVector)
 }
 
@@ -94,7 +92,6 @@ StepWiseExpression <- function(Vector){
       TempVector <- c(TempVector,CurrValue)
     }
   }
-  print(TempVector)
   return(TempVector)
 }
 
@@ -103,13 +100,171 @@ NormalExpression <- function(NumberOfProbes){
 }
 
 
+GenerateComplementaryGeneExpression <- function(Vector,Mode){
+  TempVector = c()
+  #if(Mode =="Normal"){
+  #
+  #  return(TempVector)
+  #}
 
 
-# https://thepracticalr.wordpress.com/2016/08/30/2-y-axis-plotting/
-# https://rpubs.com/riazakhan94/297778
-# https://stackoverflow.com/questions/38247907/how-to-set-the-y-range-in-boxplot-graph
-plot(c(NormalCell(1000),GainNarrowMediumAberration1(5000),NormalCell(1000)),col = "blue",  ylim=c(-2,3))
-par(new = TRUE)
-plot(NormalExpression(7000),xaxt = "n", yaxt = "n",
-     ylab = "", xlab = "", col = "gray", lty = 2)
-axis(side = 4)
+  for(i in Vector){
+
+    if(Mode == "Linear"){
+
+        TempVector = c(TempVector,LinearExpression(c(i)))
+  
+    }else if(Mode == "Sigmoid"){
+
+
+        TempVector = c(TempVector,SigmoidExpression(c(i)))
+
+    }else if (Mode=="StepWise"){
+        TempVector = c(TempVector,StepWiseExpression(c(i)))
+    }
+  }
+
+  return(TempVector)
+
+}
+
+
+CopyErrorCheck <- function(ProbeVector){
+  TempVector <- c()
+  for(i in 1:length(ProbeVector)){
+    if(round(GetCopyNumber(ProbeVector[i])) != 2){
+      TempVector <- c(TempVector,c(i))
+    }
+
+  }
+
+  return(TempVector)
+} 
+
+LossList <- function(ProbeVector){
+
+  TempVector <- c()
+  for(i in 1:length(ProbeVector)){
+    
+    if(round(GetCopyNumber(ProbeVector[i])) < 2){
+      TempVector <- c(TempVector,c(i))
+    }
+
+  }
+
+  return(TempVector)
+
+
+
+}
+
+AmplifictionList <- function(ProbeVector){
+
+  TempVector <- c()
+  for(i in 1:length(ProbeVector)){
+
+    if(round(GetCopyNumber(ProbeVector[i])) > 2){
+      TempVector <- c(TempVector,c(i))
+    }
+
+  }
+
+  return(TempVector)
+
+
+}
+
+
+# Ask for user input (which gene should get the type I,II,etc.)
+# Hold x values for each Gene Position
+GenePositionsTypeI = c() 
+GenePositionsTypeII = c()
+GenePositionsTypeIII = c()
+GenePositionsTypeIV = c()
+GenePositionsTypeV = c()
+
+NumberOfTypeI = 3
+NumberOfTypeII = 3
+NumberOfTypeIII = 3
+NumberOfTypeIV = 3
+NumberOfTypeV = 3
+
+
+
+CopyNumberExpression = c(NormalCell(100),GainNarrowMediumAberration1(500),NormalCell(100))
+
+# To help decide where to put typeI-V genes If User does not specifiy anything in terms of location
+ProbeRegionsWithCopyNumberProblems = CopyErrorCheck(CopyNumberExpression)  # For Type I - III
+ProbeRegionsWithCopyNumberLoss = LossList(CopyNumberExpression) # For Type IV
+ProbeRegionsWithCopyNumberGain = AmplifictionList(CopyNumberExpression) # For Type V
+
+
+
+NumberOfPatients = 10
+ModelType = "Sigmoid"
+TotalNumberOfProbes = length(CopyNumberExpression)
+
+# Setting probabilites of genes types:
+#TypeI appears in all patients
+#TypeII appears in 75%
+NumberofTypeIIs = round(0.75 * NumberOfPatients)
+GraphsThatWillHaveTypeIIs = c()
+for(i in 1:NumberofTypeIIs){
+  GraphsThatWillHaveTypeIIs = c(GraphsThatWillHaveTypeIIs,1)
+}
+for(i in 1:(NumberOfPatients-NumberofTypeIIs)){
+  GraphsThatWillHaveTypeIIs = c(GraphsThatWillHaveTypeIIs,0)
+}
+GraphsThatWillHaveTypeIIs = sample(GraphsThatWillHaveTypeIIs)
+
+#TypeIII appears in 50%
+NumberofTypeIIIs = round(0.50 * NumberOfPatients)
+GraphsThatWillHaveTypeIIIs = c()
+for(i in 1:NumberofTypeIIIs){
+  GraphsThatWillHaveTypeIIIs = c(GraphsThatWillHaveTypeIIIs,1)
+}
+for(i in 1:(NumberOfPatients-NumberofTypeIIIs)){
+  GraphsThatWillHaveTypeIIIs = c(GraphsThatWillHaveTypeIIIs,0)
+}
+GraphsThatWillHaveTypeIIIs = sample(GraphsThatWillHaveTypeIIIs)
+
+
+
+# Still have to add user input version! & Fix Indexes
+# https://thomasleeper.com/Rcourse/Tutorials/plotcolors.html
+for(k in 0:NumberOfPatients){
+
+      GeneExpression = GenerateComplementaryGeneExpression(CopyNumberExpression,ModelType)
+      # Color Change
+      IndexCounter = 1
+      # InsertTypeI
+      IndexestoChangeColorOfTypeI = c()
+      for(i in 0:NumberOfTypeI){
+        IndexestoChangeColorOfTypeI <- c(IndexestoChangeColorOfTypeI ,c(ProbeRegionsWithCopyNumberProblems[IndexCounter])) 
+        IndexCounter =  IndexCounter + 1
+      }
+
+      IndexestoChangeColorOfTypeII = c()
+      if(GraphsThatWillHaveTypeIIs[k+1] == 1){
+        for(i in 0:NumberOfTypeII){
+        IndexestoChangeColorOfTypeII <- c(IndexestoChangeColorOfTypeII ,c(ProbeRegionsWithCopyNumberProblems[IndexCounter])) 
+        IndexCounter =  IndexCounter + 1
+      }
+      }
+      # https://thepracticalr.wordpress.com/2016/08/30/2-y-axis-plotting/
+      # https://rpubs.com/riazakhan94/297778
+      # https://stackoverflow.com/questions/38247907/how-to-set-the-y-range-in-boxplot-graph
+
+
+  
+      xcor <- 1:TotalNumberOfProbes
+      print("Generating Graphs")
+      plot(xcor,CopyNumberExpression,col = "blue",  ylim=c(-2,3))
+      par(new = TRUE)
+      plot(xcor,GeneExpression ,xaxt = "n", yaxt = "n",
+          ylab = "", xlab = "", col=ifelse(xcor ==IndexestoChangeColorOfTypeI,"red","black"), lty = 2,ylim=c(-8,12),
+          bg=ifelse(xcor==IndexestoChangeColorOfTypeI,"red","white"))
+      axis(side = 4)
+      print("Graphs Generated.")
+      print("=======")
+}
